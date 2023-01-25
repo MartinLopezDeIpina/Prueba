60308f0a35ed31dd4986b48f745de3ed225de0fa
Admin <mlopezdeipina001@ikasle.ehu.eus>
2023-01-25 10:49:08 +0100

    7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo6");
+        System.out.println("Hola mundo7");
     }
 }
